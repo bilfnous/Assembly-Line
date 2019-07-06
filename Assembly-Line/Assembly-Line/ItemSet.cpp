33778e5af2f3f55bb69a081bfe2baa689961460e
bilfnous <b.alfanous@outlook.com>
2019-07-06 17:02:19 -0400

    ItemSet: added functions stubs and documetations

--- Assembly-Line/Assembly-Line/ItemSet.cpp
+++ Assembly-Line/Assembly-Line/ItemSet.cpp
@@ -6,11 +6,55 @@
 *	b.alfanous@outlook.com
 */
 
-#ifndef SICT_ITEMSET_H
-#define SICT_ITEMSET_H
+/*
+	ItemSet manages the stock inventory of a particular item.	
+*/
+
+#include <iostream>
+#include "ItemSet.h"
 
 namespace sict {
 
+	/*
+		A constructor that receives a reference to an unmodifiable string, 
+		extracts 4 tokens from the string, populates the object with those tokens 
+		and determines the field width to be used in displaying data for all objects of the class.
+	*/
+	ItemSet::ItemSet(const std::string*) {}
+
+	/*
+		A query that returns a reference to the name of the item
+	*/
+	const std::string& ItemSet::getName() const {}
+
+	/*
+		A query that returns the serial number of the item
+	*/
+	const unsigned int ItemSet::getSerialNumber() const {}
+
+	/*
+		A query that returns the remaining number of items in the set
+	*/
+	const unsigned int ItemSet::getSerialNumber() const {}
+
+	/*
+		a prefix decrement operator that reduces the number of items in 
+		stock by one and increases the serial number by one.
+	*/
+	ItemSet& ItemSet::operator--() {}
+
+	/*
+		a query that receives a reference to an std::ostream object os and 
+		a Boolean full and inserts the data for the current object into stream os. 
+		If the Boolean is false the data consists of the name of the items in the set 
+		and the next serial number to be assigned. If the Boolean is true, the data 
+		consists of the name, serial number quantity in stock and the description of
+		the items in the set (as shown above). The field width for the name is just
+		large enough to display the largest name. The field width for the serial number 
+		is 5 with ‘0’ fill and the field width for the quantity in stock is 3 left-justified. 
+		Fields are separated by a single blank character.
+	*/
+	void ItemSet::display(std::ostream& os, bool full) const {}
+
 }
 
-#endif
--- Assembly-Line/Assembly-Line/ItemSet.h
+++ Assembly-Line/Assembly-Line/ItemSet.h
@@ -5,3 +5,25 @@
 *	Auther B. Alfanous
 *	b.alfanous@outlook.com
 */
+
+#ifndef SICT_ITEMSET_H
+#define SICT_ITEMSET_H
+
+#include <string>
+
+namespace sict {
+
+	class ItemSet {
+
+	public:
+		ItemSet(const std::string*);
+		const std::string& getName() const;
+		const unsigned int getSerialNumber() const;
+		const unsigned int getSerialNumber() const;
+		ItemSet& operator--();
+		void display(std::ostream& os, bool full) const;
+	};
+
+}
+
+#endif
