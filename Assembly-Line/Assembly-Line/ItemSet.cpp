560919dcd8f94ee38f9a30a0c5ad3c5d368847bc
bilfnous <b.alfanous@outlook.com>
2019-07-10 22:05:26 -0400

    ItemSet module is done.

--- Assembly-Line/Assembly-Line/ItemSet.cpp
+++ Assembly-Line/Assembly-Line/ItemSet.cpp
@@ -11,6 +11,7 @@
 */
 
 #include <iostream>
+#include <iomanip>
 #include "ItemSet.h"
 
 namespace sict {
@@ -20,28 +21,67 @@ namespace sict {
 		extracts 4 tokens from the string, populates the object with those tokens 
 		and determines the field width to be used in displaying data for all objects of the class.
 	*/
-	ItemSet::ItemSet(const std::string*) {}
+	ItemSet::ItemSet(const std::string& record) {
+		//size_t next_pos = record.find(mo_utility.getDelimiter());
+		//m_name = record.substr(0, next_pos);
+
+		size_t next_pos = 0;
+		m_name = mo_utility.extractToken(record, next_pos);
+		m_serialNumber = std::stoul(mo_utility.extractToken(record, next_pos));	// extract token and convert from string to ulong
+		m_quantity = std::stoi(mo_utility.extractToken(record, next_pos));
+		m_description = mo_utility.extractToken(record, next_pos);
+
+		// update the utility fieldWidth with the largest name's length
+		if (mo_utility.getFieldWidth() < m_name.length())
+			mo_utility.setFieldWidth(m_name.length());
+	}
+
+	ItemSet::ItemSet(ItemSet && other) {
+		if (this != &other)	{
+			m_name = other.m_name;
+			m_description = other.m_description;
+			m_serialNumber = other.m_serialNumber;
+			m_quantity = other.m_quantity;
+
+			other.m_name = { "" };
+			other.m_description = { "" };
+			other.m_serialNumber = { 0 };
+			other.m_quantity = { 0 };
+		}
+	}
+
 
 	/*
 		A query that returns a reference to the name of the item
 	*/
-	const std::string& ItemSet::getName() const {}
+	const std::string& ItemSet::getName() const {
+		return m_name;
+	}
 
 	/*
 		A query that returns the serial number of the item
 	*/
-	const unsigned int ItemSet::getSerialNumber() const {}
+	const unsigned int ItemSet::getSerialNumber() const {
+		return m_serialNumber;
+	}
 
 	/*
 		A query that returns the remaining number of items in the set
 	*/
-	const unsigned int ItemSet::getSerialNumber() const {}
+	const unsigned int ItemSet::getQuantity() const {
+		return m_quantity;
+	}
 
 	/*
 		a prefix decrement operator that reduces the number of items in 
 		stock by one and increases the serial number by one.
 	*/
-	ItemSet& ItemSet::operator--() {}
+	ItemSet& ItemSet::operator--() {
+		m_quantity--;
+		m_serialNumber++;
+
+		return *this;
+	}
 
 	/*
 		a query that receives a reference to an std::ostream object os and 
@@ -54,7 +94,12 @@ namespace sict {
 		is 5 with ‘0’ fill and the field width for the quantity in stock is 3 left-justified. 
 		Fields are separated by a single blank character.
 	*/
-	void ItemSet::display(std::ostream& os, bool full) const {}
+	void ItemSet::display(std::ostream& os, bool full) const {
+		size_t fw = mo_utility.getFieldWidth();
+
+		if (full)
+			os << std::left << std::setw(fw) << m_name << " [" << std::setw(5) << m_serialNumber << "] Quantity " << std::setw(3) << m_quantity << std::right << " Description: " << m_description << std::endl;
+	}
 
 }
 
--- Assembly-Line/Assembly-Line/ItemSet.h
+++ Assembly-Line/Assembly-Line/ItemSet.h
@@ -10,16 +10,27 @@
 #define SICT_ITEMSET_H
 
 #include <string>
+#include "Utilities.h"
 
 namespace sict {
 
 	class ItemSet {
+		
+		std::string m_name;
+		std::string m_description;
+		unsigned int m_serialNumber;
+		unsigned int m_quantity;
+		Utilities mo_utility;
 
 	public:
-		ItemSet(const std::string*);
+		ItemSet(const std::string&);
+		ItemSet(ItemSet&&);
+		ItemSet(const ItemSet&) = delete;
+		ItemSet& operator=(const ItemSet&) = delete;
+		ItemSet& operator=(ItemSet&&) = delete;
 		const std::string& getName() const;
 		const unsigned int getSerialNumber() const;
-		const unsigned int getSerialNumber() const;
+		const unsigned int getQuantity() const;
 		ItemSet& operator--();
 		void display(std::ostream& os, bool full) const;
 	};
